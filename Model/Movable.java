a09e8cde4dd7588f3d30dde68d708babf3adda4f
dendyliu <dendyliu666@yahoo.com>
2016-04-28 05:19:39 +0700

    fix java doc

--- Model/Movable.java
+++ Model/Movable.java
@@ -43,13 +43,43 @@ public abstract class Movable {
         * @param m Movable object
         */
 	public abstract void interact(Movable m);
-
+        /*
+        * force to kill this object
+        */
 	public void forceKill(){ alive = false; }
+        /*
+        * Return true if this object is dead
+        * @return boolean containing isDead
+        */
 	public boolean isDead(){ return !alive; }
+         /*
+        * Return true if this object is dead
+        * @return boolean containing isDead
+        */
 	public boolean isAlive(){ return alive; }
+         /*
+        * Return true if this object is alive
+        * @return boolean containing isAlive
+        */
 	public float getAngle(){ return angle; }
+         /*
+        * Return Angle of this object
+        * @return float containing angle
+        */
 	public float getSpeed(){ return speed; }
+         /*
+        * Return speed of this object
+        * @return float containing speed
+        */
 	public float getRadius(){ return radius; }
+         /*
+        * Return posisition in coordinat x of this object
+        * @return int containing posistion in coordinat x
+        */
 	public int getX(){ return new Float(x).intValue(); }
+         /*
+        * Return posisition in coordinat y of this object
+        * @return int containing posistion in coordinat y
+        */
 	public int getY(){ return new Float(y).intValue(); }
 }
