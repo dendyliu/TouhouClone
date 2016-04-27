e648c7fb5ef51f60c06ff5a244d5832ceb328f5c
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2016-04-27 11:49:22 +0700

    Move Event initialization to main

--- Main.java
+++ Main.java
@@ -1,10 +1,66 @@
 
 import Model.Battlefield;
+import Model.Event.*;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 	public static void main(String[] args){
 		java.awt.EventQueue.invokeLater(() -> {
 			Battlefield b = new Battlefield();
+			ScheduledExecutorService scheduledPool = Executors.newScheduledThreadPool(1);
+			scheduledPool.scheduleWithFixedDelay(new PlayerFiringEvent(b, 200), 30, 20, TimeUnit.MILLISECONDS);
+			int t = 0;
+			t += 1000;
+			for (int i = 0; i < 10; ++i) {
+				scheduledPool.schedule(new RainingEvent(b, 10, 50, 0, 300), t, TimeUnit.MILLISECONDS);
+				t += 500;
+			}
+			for (int i = 0; i < 5; ++i) {
+				scheduledPool.schedule(new BoomerangShotgunEvent(b, 4, 60, 60, 4, 250), t, TimeUnit.MILLISECONDS);
+				t += 500;
+			}
+
+			for (int i = 0; i < 1; ++i) {
+				scheduledPool.schedule(new RicochetFlowerEvent(b, 100, 50, 100, 0, b.getWidth()), t, TimeUnit.MILLISECONDS);
+				t += 5000;
+			}
+
+			for (int i = 0; i < 5; ++i) {
+				scheduledPool.schedule(new SpiralEvent(b, 10, i * 360 / 5.f, 1, 0, 100), t, TimeUnit.MILLISECONDS);
+				t += 500;
+			}
+			t += 2000;
+
+			for (int i = 0; i < 10; ++i) {
+				t += 200;
+				scheduledPool.schedule(new FlowerEvent(b, 10, i * 10.f, 100), t, TimeUnit.MILLISECONDS);
+			}
+
+
+			for (int i = 0; i < 10; ++i) {
+				t += 200;
+				scheduledPool.schedule(new FlowerEvent(b, 10, -i * 10.f, 100), t, TimeUnit.MILLISECONDS);
+			}
+
+			for (int i = 0; i < 10; ++i) {
+				t += 200;
+				scheduledPool.schedule(new FlowerEvent(b, 10, i * 10.f, 100), t, TimeUnit.MILLISECONDS);
+			}
+
+			t += 1000;
+
+			for (int i = 0; i < 10; ++i) {
+				scheduledPool.schedule(new FlowerEvent(b, 10, 0.f, 100 + i * 10), t, TimeUnit.MILLISECONDS);
+				t += 100;
+			}
+
+			for (int i = 0; i < 10; ++i) {
+				scheduledPool.schedule(new ShotgunEvent(b, 4, 60f, 60f, 4, 100), t, TimeUnit.MILLISECONDS);
+				t += 500;
+			}
 		});
 
 		
--- Model/Battlefield.java
+++ Model/Battlefield.java
@@ -19,16 +19,12 @@ public class Battlefield extends JPanel {
 	private Vector<Movable> mList;
 	private AssetLoader assetLoader;
 	private JFrame frame;
-	private ScheduledExecutorService scheduledPool;
-
-
 	public Battlefield() {
 
 		width = 600;
 		height = 800;
 		mList = new Vector<>();
 		assetLoader = new AssetLoader();
-		scheduledPool = Executors.newScheduledThreadPool(1);
 
 		frame = new JFrame("Touhou Clone");
 		frame.setSize(600, 800);
@@ -37,7 +33,8 @@ public class Battlefield extends JPanel {
 
 		frame.setLayout(new BorderLayout());
 		frame.add(this, BorderLayout.CENTER);
-		
+
+
 		Boss boss = new Boss(200, 200, 2000);
 		add(boss);
 		Player player = new Player(200, 500);
@@ -46,74 +43,23 @@ public class Battlefield extends JPanel {
 
 		new Thread(task2).start();
 
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {	
-				super.windowClosing(e);
-				System.exit(0);
-			}
-		});
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		Battlefield b = this;
 		Timer timer = new Timer(20, e -> new UpdateEvent(b, 0.02f).run());
 		timer.start();
-		scheduledPool.scheduleWithFixedDelay(new PlayerFiringEvent(this, 0.2f), 30, 20, TimeUnit.MILLISECONDS);
 
-		int t = 0;
-		t += 1000;
-		for (int i = 0; i < 10; ++i) {
-			scheduledPool.schedule(new RainingEvent(this, 10, 50, 0, 300), t, TimeUnit.MILLISECONDS);
-			t += 500;
-		}
-		for (int i = 0; i < 5; ++i) {
-			scheduledPool.schedule(new BoomerangShotgunEvent(this, 4, 60, 60, 4, 250), t, TimeUnit.MILLISECONDS);
-			t += 500;
-		}
-
-		for (int i = 0; i < 1; ++i) {
-			scheduledPool.schedule(new RicochetFlowerEvent(this, 100, 50, 100, 0, frame.getWidth()), t, TimeUnit.MILLISECONDS);
-			t += 5000;
-		}
-
-		for (int i = 0; i < 5; ++i) {
-			scheduledPool.schedule(new SpiralEvent(this, 10, i * 360 / 5.f, 1, 0, 100), t, TimeUnit.MILLISECONDS);
-			t += 500;
-		}
-		t += 2000;
-
-		for (int i = 0; i < 10; ++i) {
-			t += 200;
-			scheduledPool.schedule(new FlowerEvent(this, 10, i * 10.f, 100), t, TimeUnit.MILLISECONDS);
-		}
-
-		for( int i = 0; i < 10; ++ i ){
-			t += 500;
-			scheduledPool.schedule(new FlowerEvent(this,10,-10.f,100), t, TimeUnit.MILLISECONDS);
-		}
-		
-		for( int i = 0; i < 10; ++ i ){
-			t += 500;
-			scheduledPool.schedule(new FlowerEvent(this,10,10.f,100), t, TimeUnit.MILLISECONDS);
-		}
-
-		
-		t += 1000;
-
-		for( int i = 0; i < 200; ++ i ){
-			scheduledPool.schedule(new FlowerEvent(this,10,10*i + 0.f,100), t, TimeUnit.MILLISECONDS);
-			t += 200;
-		}
-
-		for (int i = 0; i < 10; ++i) {
-			scheduledPool.schedule(new ShotgunEvent(this, 4, 60f, 60f, 4, 100), t, TimeUnit.MILLISECONDS);
-			t += 500;
-		}
 	}
-	
+
 	private static int getDistance(Movable a, Movable b){
 		int dx = a.getX() - b.getX();
 		int dy = a.getY() - b.getY();
 		return dx*dx+dy*dy;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
 	}
 	
 	public boolean isInField(int x, int y) {
@@ -217,6 +163,7 @@ public class Battlefield extends JPanel {
                 g.drawImage( assetLoader.getBulletImage(1), x - rad, y - rad,rad*2,rad*2,null );
 			}
 		}
+        
     }
 	
 	
